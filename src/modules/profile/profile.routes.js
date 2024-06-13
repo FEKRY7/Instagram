03e4c20faa139078993da17ff682ce79dbcf01b0
FEKRY7 <159729804+FEKRY7@users.noreply.github.com>
2024-06-13 19:05:23 +0300

    Update profile.routes.js

--- src/modules/profile/profile.routes.js
+++ src/modules/profile/profile.routes.js
@@ -3,12 +3,8 @@ const router = express.Router();
 const { myMulter, HME } = require("../../utils/multer.js");
 const {
   CreateProfile,
-  CreatePost,
-  getPost,
   DeleteProfile,
-  DeletePost,
   UpdateProfile,
-  UpdatePost,
   getProfile
 } = require("./profile.controller.js");
 
@@ -24,17 +20,12 @@ router.post("/CreateProfile/:id", myMulter().single("image"), CreateProfile);
 //   CreatePost
 // );
 
-router.post("/CreatePost/:id", myMulter().single("image"), HME, CreatePost);
+
 
 router.delete("/DeleteProfile/:id", DeleteProfile);
 
-router.delete("/DeletePost/:id", DeletePost);
-
 router.put("/UpdateProfile/:id", myMulter().single("image"), HME, UpdateProfile);
 
-router.put("/UpdatePost/:id", myMulter().single("image"), HME, UpdatePost);
-
-router.get("/getPost/:id", getPost);
 
 router.get("/getProfile/:id", getProfile);
 
